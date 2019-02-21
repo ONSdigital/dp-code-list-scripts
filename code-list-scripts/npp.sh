e43ec3d11d79c3589baa87ca44f1e5fd4a6d7c0f
James Bryant <jamesbryant792@gmail.com>
2019-02-21 11:33:36 +0000

    tidied up npp codelists

--- code-list-scripts/npp.sh
+++ code-list-scripts/npp.sh
@@ -2,16 +2,16 @@
 
 #This script will import required code lists for all NPP datasets
 # NPP - population numbers
-# NPP - mortality assumptions - financial years and cross border rates missing
-# NPP - migration - financial years missing
-# NPP - fertility - financial years missing
-# NPP - deaths - financial years missing
-# NPP - births - financial years missing
-# NPP - cross border rates - npp-border missing
+# NPP - mortality assumptions 
+# NPP - migration
+# NPP - fertility
+# NPP - deaths
+# NPP - births 
+# NPP - cross border rates
 
 cypher-shell < ./migration-scripts/calendar-years.cypher
 cypher-shell < ./migration-scripts/uk-only.cypher
-cypher-shell < ./cypher/npp-projection-type.cypher
-cypher-shell < ./migration-scripts/npp-age.cypher
-cypher-shell < ./migration-scripts/npp-sex.cypher
+cypher-shell < ./cypher/npp-all-projectiontype.cypher
+cypher-shell < ./cypher/npp-age.cypher
+cypher-shell < ./cypher/npp-sex.cypher
 cypher-shell < ./cypher/npp-population-measure.cypher
