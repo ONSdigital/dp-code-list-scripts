4c20cfddb9d75d16dda29604b35a91a8e559fa9f
Eleanor Deal <eleanor.deal@gmail.com>
2019-04-29 13:33:58 +0100

    Add load script for dataset related code list batches for multiple query langs

--- code-list-scripts/construction.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#This script will import required code lists for the Construction dataset
-
-cypher-shell < ./cypher/mmm-yy.cypher
-cypher-shell < ./cypher/type-of-prices.cypher
-cypher-shell < ./cypher/seasonal-adjustment.cypher
-cypher-shell < ./cypher/construction-series-type.cypher
-cypher-shell < ./cypher/construction-classifications.cypher
--- code-list-scripts/wellbeing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#This script will import required code lists for the wellbeing dataset
-
-cypher-shell < ./migration-scripts/year-ending.cypher
-cypher-shell < ./migration-scripts/wellbeing-geography.cypher
-cypher-shell < ./cypher/wellbeing-estimate.cypher
-cypher-shell < ./cypher/wellbeing-measureofwellbeing.cypher
-cypher-shell < ./cypher/yyyy-yy.cypher
